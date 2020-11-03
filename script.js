27be0947d4bfee13028543ca27ea88364c0e2010
Karolis Davli <kdavlijevas@gmail.com>
2020-11-03 22:58:33 +0000

    Dynamic CSS properties

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const grid = document.querySelector('.grid');
+const button = document.querySelector('input');
+
+let htmlStyles =
+window.getComputedStyle(document.querySelector("html"));
+let rowNum =
+parseInt(htmlStyles.getPropertyValue("--rowNum"));
 
 function createGrid() {
-  for (i = 0; i <= 15; i++) {
+  for (i = 1; i <= 16*16; i++) {
     const div = document.createElement('div');
     div.classList.add('item');
     grid.appendChild(div);
   }
 }
+
+button.addEventListener('change', createGrid);
+
+
+
+
+
+
+// Dynamic CSS values, pulling them with JS
+// HTML input tag

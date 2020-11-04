068a99b06009bb0e98d52a51d50b515266b996ca
Karolis Davli <kdavlijevas@gmail.com>
2020-11-04 09:20:04 +0000

    Functional button to create custom grid, change color on hover

--- script.js
+++ script.js
@@ -1,25 +1,94 @@
 const grid = document.querySelector('.grid');
-const button = document.querySelector('input');
+const radio = document.querySelector('input');
+const reset = document.querySelector('button');
 
 let htmlStyles =
 window.getComputedStyle(document.querySelector("html"));
 let rowNum =
 parseInt(htmlStyles.getPropertyValue("--rowNum"));
+let colNum =
+parseInt(htmlStyles.getPropertyValue("--colNum"));
 
-function createGrid() {
-  for (i = 1; i <= 16*16; i++) {
+function createGrid(x, y) {
+  // Loop to create a grid
+  for (i = 1; i <= x * y; i++) {
     const div = document.createElement('div');
     div.classList.add('item');
     grid.appendChild(div);
+    console.log(i);
   }
+
+  // Add hover status to each grid item
+  const item = document.querySelectorAll('.item');
+  item.forEach((e) => {
+    e.addEventListener('mouseover', changeColor);
+  })
+
 }
 
-button.addEventListener('change', createGrid);
+// Change color function
+function changeColor() {
+  this.classList.add('item-hover');
+}
+
+radio.addEventListener('change', createGrid);
+
+
+// Reset button
+reset.addEventListener('click', resetGrid);
+
+function resetGrid() {
+  grid.innerHTML = '';
+  colNum = prompt('How many rows?');
+  rowNum = prompt('How many columns?');
+  document.documentElement.style.setProperty("--colNum", colNum);
+  document.documentElement.style.setProperty("--rowNum", rowNum);
+  createGrid(colNum, rowNum)
+}
 
 
 
-
-
-
-// Dynamic CSS values, pulling them with JS
+// Dynamic CSS properties, pulling them with JS
 // HTML input tag
+
+// const grid = document.querySelector('.grid');
+// const radio = document.querySelector('input');
+// const reset = document.querySelector('button');
+
+// let htmlStyles =
+// window.getComputedStyle(document.querySelector("html"));
+// let rowNum =
+// parseInt(htmlStyles.getPropertyValue("--rowNum"));
+
+// function createGrid() {
+//   // Loop to create a grid
+//   for (i = 1; i <= 16*16; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('item');
+//     grid.appendChild(div);
+//   }
+
+//   // Add hover status to each grid item
+//   const item = document.querySelectorAll('.item');
+//   item.forEach((e) => {
+//     e.addEventListener('mouseover', changeColor);
+//   })
+
+// }
+
+// // Change color function
+// function changeColor() {
+//   this.classList.add('item-hover');
+// }
+
+// radio.addEventListener('change', createGrid);
+
+
+// // Reset button
+// reset.addEventListener('click', resetGrid);
+
+// function resetGrid() {
+//   grid.innerHTML = '';
+//   rowNum = prompt('How many rows?');
+// }
+

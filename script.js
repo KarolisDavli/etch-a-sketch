0ace6a72137ba17124211eea9baab2f29bb0885f
Karolis Davli <kdavlijevas@gmail.com>
2020-11-04 09:36:09 +0000

    JS comments

--- script.js
+++ script.js
@@ -50,44 +50,5 @@ function resetGrid() {
 // Dynamic CSS properties, pulling them with JS
 // HTML input tag
 
-// const grid = document.querySelector('.grid');
-// const radio = document.querySelector('input');
-// const reset = document.querySelector('button');
-
-// let htmlStyles =
-// window.getComputedStyle(document.querySelector("html"));
-// let rowNum =
-// parseInt(htmlStyles.getPropertyValue("--rowNum"));
-
-// function createGrid() {
-//   // Loop to create a grid
-//   for (i = 1; i <= 16*16; i++) {
-//     const div = document.createElement('div');
-//     div.classList.add('item');
-//     grid.appendChild(div);
-//   }
-
-//   // Add hover status to each grid item
-//   const item = document.querySelectorAll('.item');
-//   item.forEach((e) => {
-//     e.addEventListener('mouseover', changeColor);
-//   })
-
-// }
-
-// // Change color function
-// function changeColor() {
-//   this.classList.add('item-hover');
-// }
-
-// radio.addEventListener('change', createGrid);
-
-
-// // Reset button
-// reset.addEventListener('click', resetGrid);
-
-// function resetGrid() {
-//   grid.innerHTML = '';
-//   rowNum = prompt('How many rows?');
-// }
+// Updating CSS variables with input from user with a prompt
 
